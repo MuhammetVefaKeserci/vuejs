a1eb11d9ab3559f3ac66671d3d57d52dae9a0225
Muhammet Vefa Keserci <111175946+MuhammetVefaKeserci@users.noreply.github.com>
2023-06-21 16:09:44 +0300

    weather.vuejs

---
+++ src/components/WeatherP.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="weather-container">
+    <h2>weather</h2>
+    <input type="text" v-model="city" placeholder="city" />
+    <button class="weather-button" @click="getWeather">enter</button>
+    <div v-if="weatherData">
+      <p>temperature: {{ weatherData.temperature }}°C</p>
+      <p>weather description: {{ weatherData.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      city: '',
+      weatherData: null
+    }
+  },
+  methods: {
+    getWeather() {
+      fetch(`http://localhost:8080/weather/${this.city}`)
+        .then(response => response.json())
+        .then(data => {
+          this.weatherData = {
+            temperature: data.main.temp,
+            description: data.weather[0].description
+          };
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.weather-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+  background-color: #f2f2f2;
+  border-radius: 5px;
+}
+
+.weather-button {
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.weather-button:hover {
+  background-color: #33805b;
+}
+
+body {
+  background-image: url("https://example.com/bulut-resmi.jpg"); /* Bulut resminin URL'sini buraya yerleştirin */
+  background-size: cover;
+}
+</style>
